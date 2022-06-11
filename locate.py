5d6187f594d3943bc4803f10d4a37d2646f37c4d
EnzoR <e.roaldes@gmail.com>
2022-06-11 12:52:57 +0200

    modif locate

--- locate.py
+++ locate.py
@@ -1,25 +1,16 @@
-def main(): 
-    import numpy as np
-    import cv2
+import pyautogui
 
-    img = cv2.imread('image/map_dofus.png', 0)
-    template = cv2.imread('image/ble_map.png', 0)
-    h, w = template.shape
+# we try to locate the template on the screen
+try:
+    wheat_loc = pyautogui.locateOnScreen('wheat_map.png', confidence = 0.9)
 
-    methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR, cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
+# in case of failure we print the error message and we exit
+except pyautogui.ImageNotFoundException:
+    print("No wheat here.")
+    exit()
 
-    for method in methods:
-        img2 = img.copy()
+wheat_point = pyautogui.center(wheat_loc)
+# we verify that the location of the wheat is on the main screen (note on the sides)
+if wheat_point[0] >= 355 and wheat_point[0] <= 1565 and wheat_point[1] >= 50 and wheat_point[1] <= 900:
+    pyautogui.click(wheat_point[0], wheat_point[1])  
 
-        result = cv2.matchTemplate(img2, template, method)
-        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
-            location = min_loc
-        else:
-            location = max_loc
-        
-        bottom_right = (location[0] + w, location[1] + h)
-        cv2.rectangle(img2, location, bottom_right, 255, 5)
-        cv2.imshow('Match', img2)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
